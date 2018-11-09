2c69586e484082cf1958ba847759cdfee6db718a
nasgrds <steven.charles.peterson@gmail.com>
2018-11-08 18:50:00 -0700

    display_twist.py displays the twist message published by control scheme

---
+++ driverside/src/inputProc/display_twist.py
@@ -0,0 +1,16 @@
+import rospy
+import yaml
+from geometry_msgs.msg import Twist
+
+#Calls the interpretJoyMsg method of the test ControlScheme whenever a joy message is received
+def recieve (data):
+    print(data)
+
+if __name__ == "__main__":
+    try:
+        rospy.init_node("display_twist")
+        rospy.Subscriber("ControlOutput", Twist, recieve)
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            rate.sleep()
+    except rospy.ROSInterruptException: pass
